d5b2a8bf7cbb9ec53b4277baeaad2f81750d2e07
Nikita Zimin <nzeemin@gmail.com>
2015-03-13 23:49:23 +0300

    WIP: .PRINT replaced with UKNC channel 0 port output.

--- !compile.bat
+++ !compile.bat
@@ -3,4 +3,4 @@
 C:\bin\rt11\rt11.exe MACRO LEVELS.MAC
 @if exist MAIN.LST del MAIN.LST
 @if exist MAIN.OBJ del MAIN.OBJ
-C:\bin\rt11\rt11.exe MACRO/LIST:DK: MAIN.MAC
+C:\bin\rt11\rt11.exe MACRO/LIST:DK: MAIN.MAC+SYSMAC.SML/LIBRARY
