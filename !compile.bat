e75f0a3f175ac1ed97e5eae172c0acec19501337
Nikita Zimin <nzeemin@gmail.com>
2015-03-29 14:27:33 +0300

    WIP: revision and compilation date.

--- !compile.bat
+++ !compile.bat
@@ -1,6 +1,17 @@
-@rem cls
+@echo off
+
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "DATESTAMP=%YYYY%-%MM%-%DD%"
+for /f %%i in ('git rev-list HEAD --count') do (set REVISION=%%i)
+echo REV.%REVISION% %DATESTAMP%
+
+echo VERSTR:	.ASCIZ /REV.%REVISION% %DATESTAMP%/ > VERSIO.MAC
+echo	.EVEN >> VERSIO.MAC
+
+@echo on
 @if exist LEVELS.OBJ del LEVELS.OBJ
-C:\bin\rt11\rt11.exe MACRO LEVELS.MAC
 @if exist MAIN.LST del MAIN.LST
 @if exist MAIN.OBJ del MAIN.OBJ
+C:\bin\rt11\rt11.exe MACRO LEVELS.MAC
 C:\bin\rt11\rt11.exe MACRO/LIST:DK: MAIN.MAC+SYSMAC.SML/LIBRARY
